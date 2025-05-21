371bb9bcbabca2c89c861e07268e5576b3c96d43
sweepstouch957 <sweepstouchcloud@gmail.com>
2025-05-21 14:29:27 -0600

    add sub page

---
+++ src/app/win-a-car/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+function PageContent(): React.JSX.Element | null {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <h1 className="text-4xl font-bold">Win a Car</h1>
+      <p className="mt-4 text-lg">Stay tuned for more details! , Messi the goat</p>
+    </div>
+  );
+}
+
+export default PageContent;
